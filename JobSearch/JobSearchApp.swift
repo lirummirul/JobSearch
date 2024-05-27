828aa9141b8a3ced3c0079b56676a9b858c10f58
lirummirul <lada.yakovleva.2018@mail.ru>
2024-05-27 19:01:27 +0300

    Initial Commit

---
+++ JobSearch/JobSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchApp.swift
+//  JobSearch
+//
+//  Created by Lambert Lani on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
